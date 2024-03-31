0cc0a69ec701507005344e040a2d4bc94bba2362
Matilda Sutherland <64000876+matildasutho@users.noreply.github.com>
2024-03-31 14:08:23 +1100

    Update draggable function to work from class

--- InteractiveMedia2024/Week2/MatildasWorkbook/js/script.js
+++ InteractiveMedia2024/Week2/MatildasWorkbook/js/script.js
@@ -1,14 +1,20 @@
-// Make the DIV element draggable:
-dragElement(document.getElementById("code-poem"));
-dragElement(document.getElementById("f_p_a_h"));
-dragElement(document.getElementById("exitsense-btn"));
-dragElement(document.getElementById("cm-btn"));
+// Get all the elements with class name "draggable":
+var draggableElements = document.getElementsByClassName("draggable");
+
+// Apply dragElement function to each draggable element:
+for (var i = 0; i < draggableElements.length; i++) {
+  dragElement(draggableElements[i]);
+}
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (elmnt.getElementsByClassName(elmnt.className + "header")[0]) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    elmnt.getElementsByClassName(elmnt.className + "header")[0].onmousedown =
+      dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
@@ -34,8 +40,8 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
   }
 
   function closeDragElement() {
